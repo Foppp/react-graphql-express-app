f1790cd180186a2b0bca08b8d282194c23fbfba0
Yura <tt@list.ru>
2021-10-16 21:02:29 +0300

    added user input

--- schema.js
+++ schema.js
@@ -4,11 +4,24 @@ const schema = buildSchema(`
   type User {
     id: ID
     username: String
+    userlastname: String
     age: Int
   }
-  
+
+  input UserInput {
+    id: ID
+    username: String!
+    userlastname: String!
+    age: Int!
+  }
+
   type Query {
     getUsers: [User]
+    getUser(id: ID): User
+  }
+
+  type Mutation {
+    createUser(input: UserInput): User
   }
 
 `);
