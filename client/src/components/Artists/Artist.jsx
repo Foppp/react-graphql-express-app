be6708368059ab0e35797311095cb8a4d7eb32e0
Yura <tt@list.ru>
2022-02-02 22:08:49 +0200

    removed unnessesary imports

--- client/src/components/Artists/Artist.jsx
+++ client/src/components/Artists/Artist.jsx
@@ -5,10 +5,8 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import ManIcon from '@mui/icons-material/Man';
 import WomanIcon from '@mui/icons-material/Woman';
-import AlarmOnIcon from '@mui/icons-material/AlarmOn';
 import Chip from '@mui/material/Chip';
 
 const Artist = ({ artist, setCurrentId, currentId }) => {
